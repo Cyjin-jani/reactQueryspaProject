17087bf7a0c013e29f63ac0ecf9a63de6edbd6bd
yjcho <jinjanic91@gmail.com>
2022-09-11 22:16:23 +0900

    feat. first commit

---
+++ src/components/treatments/hooks/useTreatments.ts
@@ -0,0 +1,17 @@
+import { useQuery } from 'react-query';
+
+import type { Treatment } from '../../../../../shared/types';
+import { axiosInstance } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+import { useCustomToast } from '../../app/hooks/useCustomToast';
+
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
+
+export function useTreatments(): Treatment[] {
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  return data;
+}
