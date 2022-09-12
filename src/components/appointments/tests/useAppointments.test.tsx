e2a01d8bfdd094ec8a05b1f2a97dd04a61b54483
yjcho <jinjanic91@gmail.com>
2022-09-13 00:10:15 +0900

    feat. customhook test 세팅 및 useAppointments test 작성

--- src/components/appointments/tests/useAppointments.test.tsx
+++ src/components/appointments/tests/useAppointments.test.tsx
@@ -1,8 +1,27 @@
-// import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 
-// import { createWrapper } from '../../../test-utils';
+import { createQueryClientWrapper } from '../../../test-utils';
 import { useAppointments } from '../hooks/useAppointments';
 
 test('filter appointments by availability', async () => {
-  // test goes here
+  const { result, waitFor } = renderHook(useAppointments, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for the appointments to populate
+  await waitFor(() => Object.keys(result.current.appointments).length > 0);
+
+  const filteredAppointmentsLength = Object.keys(result.current.appointments)
+    .length;
+
+  // set to show all appointments
+  // hook을 변경하기 위한 act
+  act(() => result.current.setShowAll(true));
+
+  // wait for the appointments to show more data than when filtered
+  await waitFor(
+    () =>
+      Object.keys(result.current.appointments).length >
+      filteredAppointmentsLength,
+  );
 });
--- src/test-utils/index.tsx
+++ src/test-utils/index.tsx
@@ -1,11 +1,31 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
 
+// 각 테스트를 위해 unique한 queryClient를 만드는 함수
+const generateQueryClient = () => {
+  return new QueryClient();
+};
+
+export function renderWithQueryClient(
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult {
+  // 특별히 제공하는 queryClient가 없다면 새로 만들어 준다.
+  const queryClient = client ?? generateQueryClient();
+
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+}
+
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
-// export const createQueryClientWrapper = (): React.FC => {
-//   const queryClient = generateQueryClient();
-//   return ({ children }) => (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// };
+// for testing custom hooks
+export const createQueryClientWrapper = (): React.FC => {
+  const queryClient = generateQueryClient();
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
